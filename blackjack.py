dd6953e26acb90c213a814e0939e2136904dca8c
Baker Renneckar <bakerrenneckar@gmail.com>
2018-04-05 23:00:19 -0400

    Updated play_round function. game is working now

--- blackjack.py
+++ blackjack.py
@@ -2,6 +2,7 @@ import random
 
 deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
 
+
 def get_hand(deck):
 
     hand = (random.sample(deck, 2))
@@ -20,16 +21,17 @@ def total(hand):
                 total += 11
     return total
 
+
 def hit(hand, deck):
     hand.append(random.choice(deck))
-    print("You now have: " + str(hand) + "for a total of "+ str(total(hand)))
+    # print("You now have: " + str(hand) + "for a total of "+ str(total(hand)))
     return hand
-
 
 
 def get_instructions():
     command = input("Would you like to stay or hit? ")
     return command
+
 
 def check_status(hand):
     if total(hand) > 21:
@@ -38,6 +40,7 @@ def check_status(hand):
         return reset(keep_playing, hand)
     else:
         get_instructions()
+
 
 def reset(keep_playing, hand):
     if keep_playing == "y":
@@ -49,34 +52,64 @@ def reset(keep_playing, hand):
         exit()
 
 
-def run_game():
-
-    command = ""
+def play_round(deck):
+    # Setup player and dealer hand
     player_hand = get_hand(deck)
     dealer_hand = get_hand(deck)
+
+    # Print hands
+    print("You have " + str(player_hand))
+    print("The dealer is showing " + str(dealer_hand))
+
+    # Check for blackjack
+    player_blackjack = total(player_hand) == 21
+
+    if player_blackjack:
+        print("You have a blackjack\n")
+
+    # user input to resolve the hand
     while True:
-
-        print(
-            "You have " + str(player_hand) + "for a total of " +
-            str(total(player_hand)))
-        # print("The dealer is showing " + str(dealer_hand[0]))
-
-        command = get_instructions()
-        if command == "hit":
-            while total(dealer_hand) < 17:
-                hit(dealer_hand, deck)
+        chosen_action = input("Would you like to hit or stay? ")
+        if chosen_action == "hit":
             hit(player_hand, deck)
-            print(player_hand)
-            print(total(player_hand))
-            # while total(dealer_hand) < 17:
-            #     hit(dealer_hand, deck)
-        elif command == "q":
-            break
+            print("Your hand is now " + str(player_hand))
+            if total(player_hand) > 21:
+                print("You've busted")
+                return False
+            else:
+                continue
+        # Resolve dealers hand
+        if total(dealer_hand) == 21:
+            print("The dealer has blackjack")
+            if player_blackjack:
+                print("This is a push")
+                return True
+            else:
+                print("The dealer won")
+                exit()
+        while total(dealer_hand) < 17:
+            hit(dealer_hand, deck)
+            print(total(dealer_hand))
+        if total(dealer_hand) > 21:
+            print("The dealer busted, you won")
+            return True
+        else:
+            if total(player_hand) > total(dealer_hand):
+                print("You won this round")
+                return True
+            else:
+                print("The dealer won")
+                return False
 
-        # if command == "hit":
-        #     hit(player_hand, deck)
-        #     check_status(player_hand)
-        # elif command == "stay":
-        #     break
+def run_game():
+
+    while True:
+        play_round(deck)
+        play_again = input("Would you like to play again? ")
+        if play_again == "no":
+            return False
+        else:
+            continue
+
 
 run_game()
